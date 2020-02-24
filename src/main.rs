e217b3ce06682f36780cd718c8e2dbaea0cce20b
Stefan Schwarz <stefan@f2o.io>
2020-02-24 21:45:20 +0100

    stupid

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,35 @@
+// stefan@xps ~/git/foosinn/rusty-prison master* 
+// $ cargo build --release && ./target/release/rusty-prison
+//     Finished release [optimized] target(s) in 0.00s
+// load time: 311.537535ms
+// result: 585778044
+// run time: 49.542291ms
+
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::time::SystemTime;
 
 fn main() -> std::io::Result<()> {
     let f = File::open("data.txt")?;
-    let f = BufReader::new(f);
+    let mut f = BufReader::new(f);
     let mut data: Vec<i64> = vec![0; 10_000_000];
 
     let start = SystemTime::now();
-    f.lines().for_each(|line| {
-        let line = line.expect("unable to read line");
-        let mut split = line.splitn(3, ' ');
+    let mut buf = vec![];
+    while f.read_until(b'\n', &mut buf).unwrap_or(0) > 0 {
+        let mut split = buf.splitn(3, |b| *b == b' ');
         let start: usize = split
             .next().expect("no start")
-            .parse().expect("start was not a i32");
+            .iter().fold(0, |sum, digit| (sum * 10) + (*digit as usize - 48));
         let end: usize = split
             .next().expect("no end")
-            .parse().expect("end was not a i32");
-        let val: i64 = split
-            .next().expect("no val")
-            .parse().expect("val was not a i32");
+            .iter().fold(0, |sum, digit| (sum * 10) + (*digit as usize - 48));
+        let val: i64 = split.next().expect("no val")[0] as i64 - 48;
 
         data[start] = data[start] + val;
         data[end] = data[end] - val;
-    });
+        buf.clear()
+    }
     println!("load time: {:?}", start.elapsed().unwrap());
 
     let start = SystemTime::now();
